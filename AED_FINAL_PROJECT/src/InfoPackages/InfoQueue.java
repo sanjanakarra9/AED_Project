c8f5571646dc4db74a78684c4a48d6b74be6d6f0
KodandRam <114702819+KodandRam@users.noreply.github.com>
2022-12-11 04:51:04 +0530

    RequestInfo

---
+++ AED_FINAL_PROJECT/src/InfoPackages/InfoQueue.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package InfoPackages;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sonur
+ */
+public class InfoQueue {
+    
+    private ArrayList<RequestInfo> infoRequestList;
+    
+    public InfoQueue(){
+        infoRequestList= new ArrayList();
+    }
+
+    public ArrayList<RequestInfo> getInfoRequestList() {
+        return infoRequestList;
+    }
+    
+    
+    
+}
